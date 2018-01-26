4423491d11f1dfdc2d485adadc5a94e393bede2b
Kim Papa <35205556+MS17-010@users.noreply.github.com>
2018-01-26 11:18:49 -0500

    Create quiz-bank.py

---
+++ quiz-bank.py
@@ -0,0 +1,14 @@
+'''
+TODO:
+Make this a web-based quiz games
+'''
+
+# Q-1
+#What is the output of this code?
+def fact(x):
+  if x == 1:
+    return 1
+  else:
+    return x*fact(x-1)
+print(fact(3))
+#6
