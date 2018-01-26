dc95610cf6b76393a7403a0b1e2b4ae9a1db1162
Kim Papa <35205556+MS17-010@users.noreply.github.com>
2018-01-26 15:52:06 -0500

    Update quiz-bank.py

--- quiz-bank.py
+++ quiz-bank.py
@@ -4,11 +4,11 @@ Make this a web-based quiz games
 '''
 
 # Q-1
-#What is the output of this code?
+# What is the output of this code?
 def fact(x):
   if x == 1:
     return 1
   else:
     return x*fact(x-1)
 print(fact(3))
-#6
+# 6
