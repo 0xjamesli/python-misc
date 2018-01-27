29e277aa6e51e0b59c1f9f950ca797c47c14f4c3
Kim Papa <35205556+MS17-010@users.noreply.github.com>
2018-01-27 12:05:07 -0500

    Update quiz-bank.py

--- quiz-bank.py
+++ quiz-bank.py
@@ -12,3 +12,16 @@ def fact(x):
     return x*fact(x-1)
 print(fact(3))
 # 6
+
+
+# Q-2
+# What is the output of this code?
+def fb(n):
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  else:
+    return(fb(n-1) + fb(n-2))
+print(fb(5))
+# 5
